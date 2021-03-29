32163b87f237376657f0c21a88f586f93bbbc6be
Darius717 <79445427+Darius717@users.noreply.github.com>
2021-03-29 18:45:57 +0300

    Initial commit update

--- js/mainHome.js
+++ js/mainHome.js
@@ -1,5 +1,45 @@
-// IMPORT
+//simport {services} from './components/services/services.js';
+
 // header
+/*
+- Nav: 
+    LEFT- Logo kairėje + text 'Personal' (redirectina home);
+
+    RIGHT
+        - Home
+        - About
+        - Services
+        - Portfolio
+        - Pricing
+        - Blog *dropdown menu lvl 1*
+        - Blog home
+        - Blog single
+        - Pages*dropdown menu lvl 1*
+        - Elements
+        - Level 2 *dropdown menu lvl 2*
+        - Item 1
+        - Item 2
+        - Contac
+        */
+
+        /*
+- SECTION HERO:
+
+        HEROLEFT:<p><h1><p><button> discover now 
+        
+        RIGHT:img
+        */
+
+        /*
+/MAIN CONTENT/
+            //SECTION GAP
+            - SECTION: ABOUT ME
+            <p><h1><p>
+            <button> View full details -> redirectina į ABOUT vidinį puslapį.
+        */
+
+            
+
 // services
 // achievements
 // footer
