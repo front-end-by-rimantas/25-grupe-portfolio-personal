1a4017bccbed62195717719f6388e3b9f7e1345b
Vaigaudas <79445277+tonyhokas113@users.noreply.github.com>
2021-04-08 10:03:49 +0300

    Counter

--- js/components/counter.js
+++ js/components/counter.js
@@ -1,22 +1,25 @@
 const counters = document.querySelectorAll('.numberup');
+const startPoint = document.querySelector('.count');
 const counterSpeed = 150;
 
-counters.forEach(counter => {
-	const updateNumber = () => {
-		const finishNumber = +counter.getAttribute('data-target');
-		const startNumber = +counter.innerText;
-		
-		const numberUp = finishNumber / counterSpeed;
-
-		if (startNumber < finishNumber) {
-			counter.innerText = Math.ceil(startNumber + numberUp);
-			setTimeout(updateNumber, 10);
-		}
-		else {
-			counter.innerText = finishNumber;
-		}
-	};
-	updateNumber();
+startPoint.addEventListener('scroll', () => {
+	counters.forEach(counter => {
+		const updateNumber = () => {
+			const finishNumber = +counter.getAttribute('data-target');
+			const startNumber = +counter.innerText;
+			
+			const numberUp = finishNumber / counterSpeed;
+			
+			if (startNumber < finishNumber) {
+				counter.innerText = Math.ceil(startNumber + numberUp);
+				setTimeout(updateNumber, 10);
+			}
+			else {
+				counter.innerText = finishNumber;
+			}
+		};
+		updateNumber();
+	});
 });
-
+	
 export default 'counter';
