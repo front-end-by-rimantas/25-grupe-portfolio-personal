6a45af81a9e741518eac5fd48d17b9c648dd5b37
LidijaMK <79445808+LidijaMK@users.noreply.github.com>
2021-04-13 18:06:59 +0300

    plans data

---
+++ js/components/chooseYourPlan/plans.js
@@ -0,0 +1,19 @@
+function plans (selector, data) {
+    // input validation
+
+    // logic
+    const DOM = document.querySelector(selector);
+
+    if (!DOM) {
+        console.error('Pagal pateikta selektoriu nerastas norimas elementas');
+        return false;
+    }
+
+  
+
+    //post logic validation
+
+    // result return
+}
+
+export { plans }
--- js/mainHome.js
+++ js/mainHome.js
@@ -9,6 +9,8 @@ import { achievementsData } from './data/achievementsData.js'
 /* PORTFOLIO */
 /* TESTIMONIALS */
 /* PRICE AREA */
+import { plansData } from './data/plansData.js';
+import { plans } from './components/chooseYourPlan/plans.js'
 /* RECENT BLOG */
 import { blogData } from './data/blogData.js';
 import { blog } from './components/blog/blog.js';
@@ -27,6 +29,7 @@ const achievement = new Achievements('#achievements_block',achievementsData);
 /* PORTFOLIO */
 /* TESTIMONIALS */
 /* PRICE AREA */
+plans('#plans_block', plansData);
 /* RECENT BLOG */
 blog('#blog-block', blogData);
 /* BRANDS AREA */
