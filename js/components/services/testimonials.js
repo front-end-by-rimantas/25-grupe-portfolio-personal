9eb1066d6d039a8ac4d75ab149105827d04175bd
Vaigaudas <79445277+tonyhokas113@users.noreply.github.com>
2021-04-13 16:20:44 +0300

    Html +=

--- js/components/services/testimonials.js
+++ js/components/services/testimonials.js
@@ -5,9 +5,9 @@ function testimonials(selector, testData) {
 
   let HTML = '';
   let testCount = 0;
-  let scrSize = window.matchMedia('(min-width: 767px)');
 
-  function hideTestimonial(scrSize) {
+  let scrSize = window.matchMedia('(min-width: 767px)');
+  function hideTestimonial(screenInfo) {
     if (scrSize.matches) {
       testMaxCount = 2;
     }
@@ -15,9 +15,10 @@ function testimonials(selector, testData) {
       testMaxCount = 1;
     }
   }
-  
-  document.addEventListener('resize', hideTestimonial);
+  window.addEventListener("resize", hideTestimonial);
   hideTestimonial(scrSize);
+  console.log(scrSize);
+  
 
   for (let i = 0; i < testList.length; i++) {
       const testimonial = testList[i];
@@ -25,29 +26,24 @@ function testimonials(selector, testData) {
       if (!testimonial.active) {
           continue;
       } 
-      
       if (testCount === testMaxCount) {
         break;
       }
 
       testCount++;
-      HTML += 
-      `<div class="testimonial-box col-12 col-md-6 col-lg-6">
-      <img
-      class="mini-logo"
-      src="${testImgPath + testimonial.img}"
-      alt="User logo"
-      height="33px"
-      width="33px"
-      />
-      <div class="text-box">
-      <p class="subtitle">
-      ${testimonial.paragraph}
-      </p>
-      <h1 class="header header-hover">${testimonial.header}</h1>
-      <p class="subtitle">${testimonial.secondParagraph}</p>
-      </div>
-      </div>`;
+      HTML += `<div class="testimonial-box">
+                <img class="mini-logo"
+                  src="${testImgPath + testimonial.img}"
+                  alt="User logo"
+                  height="33px"
+                  width="33px"
+                />
+                <div class="text-box">
+                  <p class="subtitle"> ${testimonial.paragraph} </p>
+                  <h1 class="header header-hover">${testimonial.header}</h1>
+                  <p class="subtitle">${testimonial.secondParagraph}</p>
+                </div>
+               </div>`;
     }
     
 DOM.innerHTML = HTML;
