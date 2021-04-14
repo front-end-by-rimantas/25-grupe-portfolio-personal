1023a67c1f9d07c9eec6cc55e9565249eefd35d7
Eglė Balčiūnaitė <balciunaitegle@gmail.com>
2021-04-14 11:55:33 +0300

    small media layout update

--- js/components/gallery/gallery.js
+++ js/components/gallery/gallery.js
@@ -5,13 +5,22 @@ function gallery(selector, data) {
 
   const DOM = document.querySelector(selector);
   const galleryArray = data.list;
+  const imgPath = data.imgPath;
   let HTML = "";
-  for (let i = 0; i < data.list.length; i++) {
-    HTML += `<div class="filterDiv vector col-12 col-sm-4 col-md-4 col-xl-2 ml-xl-10">
-          <img src="./img/gallery/p1.jpg" alt="vinylDesignVector" />
+  for (let i = 0; i < galleryArray.length; i++) {
+    const galleryItem = galleryArray[i];
+
+    if (!galleryItem.active) {
+      continue;
+    }
+    HTML += `<div class="filterDiv vector col-12 col-md-4 col-xl-2 ml-xl-10">
+          <img src="${imgPath + galleryItem.img}" alt="vinylDesignVector" />
+          <i class="fa fa-binoculars"></i>
           <div class="galleryInner">
-            <div class="row center"><h3>2d vinyl design</h3></div>
-            <div class="row gallerySubtitle center">vector</div>
+            <div class="row inGalleryTitle center"><p>${
+              galleryItem.title
+            }</p></div>
+            <div class="row inGallerySubtitle center">${galleryItem.type}</div>
           </div>
           </div>
           </div>`;
