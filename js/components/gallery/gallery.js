20c9a535abbbeed8888d3b836bc780419faabc57
Eglė Balčiūnaitė <balciunaitegle@gmail.com>
2021-04-14 16:38:18 +0300

    gallery-validation

--- js/components/gallery/gallery.js
+++ js/components/gallery/gallery.js
@@ -1,18 +1,36 @@
+import { isValidGalleryPost } from "./isValidGalleryPost.js";
+import { isValidGalleryItem } from "./isValidGalleryItem.js";
+
 function gallery(selector, data) {
-  console.log(data);
-  //input validation
-  // logic
+  if (!isValidGalleryPost(selector, data)) {
+    return false;
+  }
 
   const DOM = document.querySelector(selector);
+
+  if (!DOM) {
+    console.error("Pagal pateikta selektoriu nerastas norimas elementas");
+    return false;
+  }
+
   const galleryArray = data.list;
   const imgPath = data.imgPath;
+  const maxCount = data.maxCount;
+
   let HTML = "";
+  let generatedGalleryCount = 0;
+
   for (let i = 0; i < galleryArray.length; i++) {
     const galleryItem = galleryArray[i];
 
-    if (!galleryItem.active) {
+    if (!isValidGalleryItem(galleryItem) || !galleryItem.active) {
       continue;
     }
+    if (generatedGalleryCount === maxCount) {
+      break;
+    }
+    generatedGalleryCount++;
+
     HTML += `<div class="filterDiv vector col-12 col-md-4 col-xl-2 ml-xl-10">
           <img src="${imgPath + galleryItem.img}" alt="vinylDesignVector" />
           <i class="fa fa-binoculars"></i>
@@ -20,15 +38,15 @@ function gallery(selector, data) {
             <div class="row inGalleryTitle center"><p>${
               galleryItem.title
             }</p></div>
-            <div class="row inGallerySubtitle center">${galleryItem.type}</div>
+            <div class="row inGallerySubtitle center">${
+              galleryItem.description
+            }</div>
           </div>
           </div>
           </div>`;
   }
-  // post logic validation
-  // result return
+
   DOM.innerHTML = HTML;
-  console.log(HTML);
 }
 
 export { gallery };
